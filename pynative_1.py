babb14895efc0d99f404453c8b9c77108df52b5b
djanfelipe <djan3105@gmail.com>
2020-04-17 23:34:54 -0300

    Alteração do idioma do exercício

--- pynative_1.py
+++ pynative_1.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-print('Printing Array\n')
+print('Impressão do array\n')
 
 lista = np.empty([4, 2], dtype=np.uint16)
 print(f'{lista}\n')
 
-print('Printing numpy array Atributes\n')
+print('Impressão dos atributos do array numpy\n')
 
-print(f'1>. Array shape is: {lista.shape}')
-print(f'2>. Array dimensions are {lista.ndim}')
-print(f'3>. Length of each element of array in bytes is {lista.itemsize}')
+print(f'1>. A forma do array é: {lista.shape}')
+print(f'2>. As dimensões do array são: {lista.ndim}')
+print(f'3>. O tamanho de cada elemento do array em bytes é: {lista.itemsize}')
