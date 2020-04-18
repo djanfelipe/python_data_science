3ccd7e3d5b574528fa5c176122155f386a98a206
djanfelipe <djan3105@gmail.com>
2020-04-17 23:57:02 -0300

    Alteração do idioma do exercício

--- pynative_3.py
+++ pynative_3.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-
 lista = np.array([[11,22,33],[44, 55, 66], [77, 88, 99]])
-print('Printing Input Array\n')
+print('Impressão do array de entrada\n')
 print(f'{lista}\n')
 
-print('Printing array of items in the third column of all rows\n')
+print('Impressão de array dos itens na terceira coluna de todas as linhas\n')
 
 lista_tratada = lista[:, 2]
 print(lista_tratada)
