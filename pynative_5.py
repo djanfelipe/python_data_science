cc3b5ce7208f85af0659848a72bc44286b3d23df
djanfelipe <djan3105@gmail.com>
2020-04-18 00:05:20 -0300

    Alteração do idioma do exercício

--- pynative_5.py
+++ pynative_5.py
@@ -3,10 +3,10 @@ import numpy as np
 lista1 = np.array([[5, 6, 9], [21, 18, 27]])
 lista2 = np.array([[15, 33, 24], [4, 7, 1]])
 
-print('Addition of two arrays is\n')
+print('A adição de dois arrays resulta em:\n')
 
 lista_final = lista1 + lista2
 print(f'{lista_final}\n')
 
-print('Result array after calculating the square of all elements\n')
+print('Array resultante após calcular a exponênciação de todos os elementos\n')
 print(lista_final ** 2)
