c9ffe4bb269218c61b0feb0791662d4ceab202a4
djanfelipe <djan3105@gmail.com>
2020-04-17 23:52:27 -0300

    Alteração do idioma do exercício

--- pynative_2.py
+++ pynative_2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print('Creating 5X2 array using numpy.arange\n')
+print('Criação de array 5X2 com o uso do numpy.arange\n')
 
 lista = np.array(np.arange(100, 200, 10)).reshape(5, 2)
-print(f'{lista}')
+print(lista)
